cf2e99cc2151ee12c48beb1d0c7d816faaa7afae
whitebeard19 <basith2309004@gmail.com>
2025-09-17 09:05:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './components/Header'
+import Entry from './components/Entry'
+import data from "./data"
+
+export default function App() {
+
+  const entryElements = data.map((entry) => 
+  {
+    return (
+      <Entry 
+        key={entry.id}
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    )
+  })
+
+  return (
+    <div>
+      <Header />
+      <main className='container'>
+        {entryElements}
+      </main>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+        <header>
+            <img src="globe.png" alt="globe image" />
+            <h1>my travel journal.</h1>
+        </header>
+  )
+}
